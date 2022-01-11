cd7dd4fa93385787deb75febfce3d623e5ce10d2
indrabsudirman <indrabsudirman@gmail.com>
2022-01-11 16:29:12 +0700

    Finished on Header UI, Home page

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,5 +1,6 @@
 import Logo from './logo.png'
 import SplashScreen from './splashscreenbg.png'
+import ImageHeader from './header.png'
 
 
-export { Logo, SplashScreen }
+export { Logo, SplashScreen, ImageHeader }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,44 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { Dimensions, ImageBackground, StyleSheet, Text, View } from 'react-native'
+import { ImageHeader } from '../../assets'
 
 const Home = () => {
     return (
-        <View>
-            <Text>Beranda</Text>
+        <View style={styles.page}>
+            <ImageBackground source={ImageHeader} style={styles.header}>
+                <View style={styles.welcome}>
+                    <Text style={styles.welcome_text}>Welcome,</Text>
+                    <Text style={styles.username}>Indra Sudirman</Text>
+                </View>
+            </ImageBackground>
         </View>
     )
 }
 
 export default Home
 
-const styles = StyleSheet.create({})
+const windowWidth = Dimensions.get('window').width
+const windowHeight = Dimensions.get('window').height
+
+const styles = StyleSheet.create({
+    page: {
+        flex: 1
+
+    },
+    header: {
+        width: windowWidth,
+        height: windowHeight * 0.3,
+    },
+    welcome: {
+        marginTop: windowHeight * 0.03,
+        paddingHorizontal: 15
+    },
+    welcome_text: {
+        fontSize: 20,
+        fontFamily: 'Comfortaa-Light'
+    },
+    username: {
+        fontSize: 22,
+        fontFamily: 'Comfortaa-Bold'
+    }
+})
